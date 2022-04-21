9625b90f7d76f030d83d133ee69bae18b80216f2
Hisham-Tariq <Hishamtariq.hi@gmail.com>
2022-04-21 18:37:18 +0500

    Path Modified

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ def read_key():
 
 def store_key(key):
     with open('.license.silo', 'wb') as key_file:
-        key_file.write(key.encode())  
+        key_file.write(key.encode())
 
 def check_key_validation(key):
-    result = subprocess.Popen(["/usr/bin/java", "-jar", "usr/share/silo-license-validator/validator.jar", key], stdout=subprocess.PIPE).communicate()[0]
+    result = subprocess.Popen(["/usr/bin/java", "-jar", "/var/www/license_dashboard/validator.jar", key], stdout=subprocess.PIPE).communicate()[0]
     return int(result)
 
 def get_ip_address():
@@ -30,21 +30,22 @@ def get_ip_address():
     return s.getsockname()[0];
 
 def stop_all_services():
-    # os.system("systemctl stop wazuh-manager")
-    os.system("systemctl stop kibana")
-    # os.system("systemctl stop filebeat")
-    # os.system("systemctl stop elasticsearch")
+    # os.system("/bin/systemctl stop wazuh-manager")
+    os.system("/bin/systemctl stop kibana.service")
+    # os.system("/bin/systemctl stop filebeat")
+    # os.system("/bin/systemctl stop elasticsearch")
 
 def start_all_services():
-    # os.system("systemctl start wazuh-manager")
-    os.system("systemctl start kibana")
-    # os.system("systemctl start filebeat")
-    # os.system("systemctl start elasticsearch")
+    # os.system("/bin/systemctl start wazuh-manager")
+    os.system("/bin/systemctl start kibana.service")
+    # os.system("/bin/systemctl start filebeat")
+    # os.system("/bin/systemctl start elasticsearch")
 
 @app.route("/activate", methods=['GET', 'POST'])
 def activate_dashboard():
     if exists('.license.silo'):
         return redirect(url_for('redirect_to_dashboard'))
+    stop_all_services()
     if request.method == 'POST':
         license_key = request.form['license_key']
         try:
@@ -61,7 +62,7 @@ def activate_dashboard():
     return render_template("activate.html")
 
 @app.route("/redirect")
-def redirect_to_dashboard():        
+def redirect_to_dashboard():
     key = read_key()
     if key is None:
         return redirect(url_for('activate_dashboard'))
@@ -84,9 +85,8 @@ def index():
     if exists('.license.silo'):
         return redirect(url_for('redirect_to_dashboard'))
     else:
-        return redirect(url_for('activate_dashboard'))    
+        return redirect(url_for('activate_dashboard'))
 
 if __name__ == "__main__":
     stop_all_services()
     app.run(host='0.0.0.0')
-
